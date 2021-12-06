af9ee4687665e44a2cda89ba369b626f4b49fee6
Bastien BORDEREAU <bbordere@e2r3p9.clusters.42paris.fr>
2021-12-06 17:12:41 +0100

    v 1.5.0

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bbordere <bbordere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 16:39:28 by bbordere          #+#    #+#             */
-/*   Updated: 2021/12/02 10:48:24 by bbordere         ###   ########.fr       */
+/*   Updated: 2021/12/02 12:53:42 by bbordere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void		*alloc;
 
-	alloc = malloc(count * size);
-	if (!alloc)
-		return (NULL);
 	if (count == 0 || size == 0)
 	{
 		count = 1;
 		size = 1;
 	}
+	alloc = malloc(count * size);
+	if (!alloc)
+		return (NULL);
 	ft_bzero(alloc, count * size);
 	return (alloc);
 }
--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,21 @@
 /*   By: bbordere <bbordere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 14:23:23 by bbordere          #+#    #+#             */
-/*   Updated: 2021/12/01 11:44:12 by bbordere         ###   ########.fr       */
+/*   Updated: 2021/12/02 12:02:05 by bbordere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	ft_free_all(char **tab, int i)
+{
+	while (i >= 0)
+	{
+		free(tab[i]);
+		i--;
+	}
+	free(tab);
+}
 
 static int	ft_words_count(char const *str, char sep)
 {
@@ -44,20 +54,26 @@ static int	ft_word_len(char const *str, char sep, size_t index)
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
+	char	*temp;
 	size_t	i2;
 	int		i;
 
-	i = 0;
+	i = -1;
 	i2 = 0;
 	res = malloc((ft_words_count(s, c) + 1) * sizeof(char *));
 	if (!res)
 		return (NULL);
-	while (i < ft_words_count(s, c))
+	while (++i < ft_words_count(s, c))
 	{
 		while (s[i2] && s[i2] == c)
 			i2++;
-		res[i] = ft_substr(s, i2, ft_word_len(s, c, i2));
-		i++;
+		temp = ft_substr(s, i2, ft_word_len(s, c, i2));
+		if (!temp)
+		{
+			ft_free_all(res, i);
+			return (NULL);
+		}
+		res[i] = temp;
 		i2 += ft_word_len(s, c, i2);
 	}
 	res[i] = NULL;
